0038a31ad75199c4b9d1cddef8373663543685db
Simon Walker <github@stwalkerster.co.uk>
2013-01-03 20:31:19 +0000

    updates to turtle script for turtle refueling

--- quarry.lua
+++ quarry.lua
@@ -1,6 +1,6 @@
-local length = 16; -- how far back to go minus 2
-local width = 8; -- how wide minus 2, div 2
-local depth = 16; -- how many levels (broken)
+local length = 14; -- how far back to go minus 2
+local width = 7; -- how wide minus 2, div 2
+local depth = 3; -- how many levels (broken)
 
 --[[ area to be mined:
 
@@ -24,28 +24,54 @@ local depth = 16; -- how many levels (broken)
 	
 	place turtle on the X. ]]
 
--- various functions to be used	
+-- various functions to be used
+function writeline(data)
+	term.write(data);
+	term.scroll(1);
+	oldx, oldy = term.getCursorPos()
+	term.setCursorPos(1,oldy)
+end
+	
 function tlDig()	
 	while turtle.detect() do
 		turtle.dig();
 	end
 end
 
-function tlDigDown()	
+function tlReallyDigDown()	
 	while turtle.detectDown() do
 		turtle.digDown();
 	end
 end
 
 function tlDigForward()
-	while not turtle.forward() do
-		tlDig();
+	moved = turtle.forward();
+	
+
+	while not moved do
+		if turtle.detect() then
+			tlDig();
+			moved = turtle.forward()
+		elseif turtle.getFuelLevel() == 0 then
+			turtle.select(1);
+			turtle.refuel();
+			if turtle.getFuelLevel() == 0 then
+				writeline("Please refuel me!");
+				while turtle.getFuelLevel() == 0 do
+					turtle.refuel();
+				end
+			end
+			
+			moved = turtle.forward();
+		else
+			moved = turtle.forward();
+		end
 	end
 end
 
 function tlDigDown()
 	while not turtle.down() do
-		tlDigDown();
+		tlReallyDigDown();
 	end
 end
 
@@ -59,7 +85,7 @@ end
 for d = 0, depth do
 
 	for w = 0, width do
-
+		writeline( "w = " .. w );
 		for l = 0, length do
 			tlDigForward();
 		end
