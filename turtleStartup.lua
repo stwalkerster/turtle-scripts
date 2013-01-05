263601ca16e4d283b308cfa600293a8739564dda
Simon Walker <github@stwalkerster.co.uk>
2013-01-05 19:38:45 +0000

    turtle startup fixes

--- turtleStartup.lua
+++ turtleStartup.lua
@@ -1,7 +1,12 @@
 print( "This is " .. os.getComputerLabel() )
 print("Loading APIs...")
 os.loadAPI("/git/apis/tl")
-print("Done")
+print("Connecting to WiFi: ScimonsHouse")
 tl.open()
-
+print("Checking turtle state...")
+x,y,z = gps.locate(2,false)
+print("Location: " .. x .. "," .. y .. "," .. z .. ", Fuel:" .. turtle.getFuelLevel());
+print("Updating control server...")
 tl.updateLocation("Ready.")
+print("")
+print(os.getComputerLabel() .. " ready for orders.")
