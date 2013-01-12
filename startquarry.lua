ad5c8dcdfcf36b8d8c77aa92397ab094a57517eb
root <root@spearow.lon.stwalkerster.net>
2013-01-12 19:09:45 +0000

    fixes

--- startquarry.lua
+++ startquarry.lua
@@ -8,9 +8,21 @@ while turtle.getFuelLevel() > 1000 do
 	x,y = term.getCursorPos()
 	term.setCursorPos(1,y+5);
 
+	shell.run("git/q-home.lua",  homex .. " " .. homey .. " " .. homez);
+
+	shell.run("git/q-deploy.lua")
+	turtle.down()
+
+	shell.run("git/quarry.lua")
+	x,y = term.getCursorPos()
+	term.setCursorPos(1,y+5);
+
+
 	print("Updating lighting...")
 	-- update lighting
 	shell.run("git/q-placetorch.lua")
 
 	shell.run("git/q-home.lua",  homex .. " " .. homey .. " " .. homez);
+
+
 end
